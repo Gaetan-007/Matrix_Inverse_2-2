182ba923e5f7f6a6002304ebfcd168f9d1c3d6f8
Zehao Wang <64057386+Gaetan-007@users.noreply.github.com>
2022-11-09 21:22:51 +0800

    Add files via upload

---
+++ simulation/get_first_amp.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2022/10/24 15:35:07
+// Design Name: 
+// Module Name: get_first_amp
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module get_first_amp(
+
+    );
+endmodule
---
+++ simulation/multiplier.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2022/10/24 20:10:10
+// Design Name: 
+// Module Name: multiplier
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module multiplier(
+
+    );
+endmodule
